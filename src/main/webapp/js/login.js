d67c0269aa7d902dab6b346a26502a46aba2191d
Kostennik <kostennik@gmail.com>
2020-01-06 18:27:08 +0100

    added script for login page

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,34 @@
+import {writeCookie} from "./index.js";
+import {readCookie} from "./index.js";
+
+function redirectLastPage() {
+  if (readCookie('userType') != null && readCookie('userName')) {
+    window.location.href = readCookie('userType') + '.html';
+  }
+}
+
+$(document).ready(() => {
+  //Redirect to the last page.
+  redirectLastPage();
+
+  //only focus in text input
+  document.onclick = (event) => {
+    document.getElementById("name").focus();
+  };
+
+  //write to text input User's name from Cookies
+  if (readCookie('userName')) {
+    $("#name").val(readCookie('userName'));
+  }
+
+  //send data, redirect in page
+  $("form").submit(function () {
+    writeCookie('userName', $("#name").val(), 30);
+    writeCookie('userType', $('input[name=iam]:checked').val(), 30);
+    if (readCookie('userType') != null) {
+      window.location.href = readCookie('userType') + '.html';
+    }
+    //disable standard form`s behavior
+    return false;
+  });
+});
