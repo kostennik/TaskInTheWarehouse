f212b8dbc984652eb304832dc8d81a2b6cfab3de
Kostennik <kostennik@gmail.com>
2020-01-06 21:26:26 +0100

    added handler POST

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -22,6 +22,28 @@ public class Servlet extends HttpServlet {
         res.setContentType("application/json");
         res.setCharacterEncoding("UTF-8");
         out.print(tasksJsonString);
+        System.out.println(tasksJsonString);
+        out.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String descriptionTask = req.getParameter("description_task");
+        String priority = req.getParameter("priority");
+        if (priority == null){
+            priority = "no";
+        }
+        int quantity = Integer.parseInt(req.getParameter("quantity"));
+
+
+        Task task = new Task(username, descriptionTask, priority, quantity);
+        TaskDB.insert(task);
+        System.out.println(task.toString());
+        PrintWriter out = res.getWriter();
+        res.setContentType("text/html");
+        out.print("200"); //send status
         out.flush();
     }
 }
